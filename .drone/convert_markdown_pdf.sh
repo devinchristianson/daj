5b968d7b9aaf9a075175aa9be49a2a5fa1d4d40d
devinchristianson <devin.christianson@maine.edu>
2020-02-18 16:34:17 -0500

    add renamed convert script

---
+++ .drone/convert_markdown_pdf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "Waiting on convert PDF API"
+while ! nc -z convert 3000; do
+  sleep 0.1;
+done
+echo "convert API is up"
+mkdir -p ../pdf;
+rm ../pdf/*
+shopt -s nullglob;
+for f in *.md
+do
+  echo "Processing $f file...";
+  FNAME=$(echo $f | sed s/\.md//g);
+  # take action on each file. $f store current file name
+  echo "<!doctype html>
+ <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>$FNAME</title>
+  </head>
+  <body>
+    {{ toHTML .DirPath \"$f\" }}
+  </body>
+ </html>" > index.html;
+curl --request POST \
+    --url http://convert:3000/convert/markdown \
+    --header 'Content-Type: multipart/form-data' \
+    --form files=@index.html \
+    --form files=@$f \
+    -o ../pdf/$FNAME.pdf;
+curlexit=$?
+if [ $curlexit -ne 0 ]
+then 
+exit $curlexit
+fi
+done
+rm index.html
