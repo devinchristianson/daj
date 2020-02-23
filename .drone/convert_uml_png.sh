2551975e2d6a891c842a02e61d9675d315d7e146
devinchristianson <devin.christianson@maine.edu>
2020-02-23 11:06:23 -0500

    fix newline

--- .drone/convert_uml_png.sh
+++ .drone/convert_uml_png.sh
@@ -14,8 +14,8 @@ do
   echo "Processing $f file...";
   FNAME=$(echo $f | sed s/\.pu//g);
   # take action on each file. $f store current file name
-  curl http://convertuml:8000/plantuml/png 
-    --data-binary \'@$f\'
+  curl http://convertuml:8000/plantuml/png \
+    --data-binary \'@$f\' \
     -o ../assets/uml/$FNAME.png;
 curlexit=$?
 if [ $curlexit -ne 0 ]
