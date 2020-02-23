6df81e61fafc62f4db98027d44e8d8bc182666f4
devinchristianson <devin.christianson@maine.edu>
2020-02-23 10:32:32 -0500

    update drone to add uml conversion

---
+++ .drone/convert_uml_png.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Waiting on convert UML API"
+while ! nc -z convertuml 3000; do
+  sleep 0.1;
+done
+echo "convert UML API is up"
+mkdir -p ../assets/uml;
+rm ../assets/uml/*
+shopt -s nullglob;
+for f in *.pu
+do
+  echo "Processing $f file...";
+  FNAME=$(echo $f | sed s/\.pu//g);
+  # take action on each file. $f store current file name
+  curl http://convertuml:8000/plantuml/png 
+    --data-binary \'@$f\'
+    -o ../assets/uml/$FNAME.png;
+curlexit=$?
+if [ $curlexit -ne 0 ]
+then 
+exit $curlexit
+fi
+done
